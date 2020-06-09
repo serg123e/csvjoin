cb6c3d1e5c49a94a5633cf95e762cb47b89d50ce
Sergey Evstegneiev <serg123e@gmail.com>
2020-06-09 16:36:43 +0200

    can parse column param

--- spec/compare_spec.rb
+++ spec/compare_spec.rb
@@ -1,7 +1,7 @@
 ﻿require_relative 'spec_helper'
 require File.join(File.dirname(__FILE__), '..', 'lib', 'comparator.rb')
 
-
+module Tabalmer
 describe 'tabalmer' do
   it 'finds the difference' do
 
@@ -16,7 +16,7 @@ describe 'tabalmer' do
         "Best,2020-05-01,200.0\n"+
         "Gest,2020-05-06,100.0"
 
-    expect( Tabalmer::Comparator.compare(t1, t2) ).to eq(
+    expect( Comparator.compare(t1, t2) ).to eq(
           "id,name,price,diff,table2.name,payment_date,amount\n"+
           "11,Test,100.0,<=>,Test,2020-04-15,97.0"+
           "12,Fest,150.0,==>,,,"+
@@ -24,11 +24,19 @@ describe 'tabalmer' do
           ",,,<==,Gest,2020-05-06,100.0)" )
 
   end
+  it 'can parse column param' do
+    c = Comparator.new
+    c.set_columns_to_compare("client:name,amount:price")
+    expect( c.columns ).to eq( [['client','name'], ['amount','price']] )
+  end
+
   it 'works with tsv and csv' do
   end
   it 'works with multiline csv' do
   end
   it 'works with multiline csv' do
   end
+
+end
 
 end
