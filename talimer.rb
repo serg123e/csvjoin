8c7e9cc4452d5efab5a3865e55bba1c3753440a6
Sergey Evstegneiev <serg123e@gmail.com>
2020-06-10 18:52:23 +0200

    bin

---
+++ talimer.rb
@@ -0,0 +1,10 @@
+﻿require_relative 'lib/comparator.rb'
+
+c = Talimer::Comparator.instance
+t1 = ARGV.shift
+t2 = ARGV.shift
+params = ARGV.shift
+
+      c.set_columns_to_compare(params)
+      res = c.compare(t1, t2)
+puts res
