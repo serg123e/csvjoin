026287357a34659f8dd577f41f6d621b54947027
Sergey Evstegneiev <serg123e@gmail.com>
2020-06-13 07:00:49 +0200

    callbacks in separate file

---
+++ lib/callbacks.rb
@@ -0,0 +1,33 @@
+# Callbacks for Diff::LCS with insert in left "<" + insert in right ">" instead of replace (!)
+module Diff
+  module LCS
+    class NoReplaceDiffCallbacks
+      # Returns the difference set collected during the diff process.
+      attr_reader :diffs
+
+      #:yields self:
+      def initialize
+        @diffs = []
+        yield self if block_given?
+      end
+
+      def match(event)
+        @diffs << Diff::LCS::ContextChange.simplify(event)
+      end
+
+      def discard_a(event)
+        @diffs << Diff::LCS::ContextChange.simplify(event)
+      end
+
+      def discard_b(event)
+        @diffs << Diff::LCS::ContextChange.simplify(event)
+      end
+
+      def change(event)
+        discard_a(Diff::LCS::ContextChange.new("<", event.old_position, event.old_element, nil, nil))
+        discard_b(Diff::LCS::ContextChange.new(">", nil, nil, event.new_position, event.new_element))
+        # @diffs << Diff::LCS::ContextChange.simplify(event)
+      end
+    end
+  end
+end
--- lib/comparator.rb
+++ lib/comparator.rb
@@ -1,42 +1,12 @@
 # frozen_string_literal: true
 
 require_relative 'data_row'
+require_relative 'callbacks'
 require 'diff/lcs'
-# require 'debug'
 
-# Callbacks for Diff::LCS with insert in left "<" + insert in right ">" instead of replace (!)
-class Diff::LCS::NoReplaceDiffCallbacks
-  # Returns the difference set collected during the diff process.
-  attr_reader :diffs
-
-  #:yields self:
-  def initialize
-    @diffs = []
-    yield self if block_given?
-  end
-
-  def match(event)
-    @diffs << Diff::LCS::ContextChange.simplify(event)
-  end
-
-  def discard_a(event)
-    @diffs << Diff::LCS::ContextChange.simplify(event)
-  end
-
-  def discard_b(event)
-    @diffs << Diff::LCS::ContextChange.simplify(event)
-  end
-
-  def change(event)
-    discard_a(Diff::LCS::ContextChange.new("<", event.old_position, event.old_element, nil, nil))
-    discard_b(Diff::LCS::ContextChange.new(">", nil, nil, event.new_position, event.new_element))
-    # @diffs << Diff::LCS::ContextChange.simplify(event)
-  end
-end
 
 module CSVJoin
   LEFT = 1 # 'left'
-
   RIGHT = 2 # 'right'
 
   # Compare and join two tables
